ac899ca25842bf6fc66ad3f5ba20cbbc3b319759
gamer <95480971+NOMAUR@users.noreply.github.com>
2025-04-07 13:35:28 -0400

    router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
  <Contador></Contador>
+ 
+ <MainView></MainView>
+ <ListaDeTareas></ListaDeTareas>
 </template>
 
 
 <script setup>
 import Contador from './components/Contador.vue';
+import MainView from './views/MainView.vue';
+import ListaDeTareas from './components/ListaDeTareas.vue';
 
 </script>
 
---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(tareas, index) in tareas" :key="index">
+                {{ tareas }} 
+                <button @click="deleteTarea(index)">Eliminar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const addTarea = () => {
+    tareas.value.push(nuevaTarea.value);
+    nuevaTarea.value = '';
+}
+const deleteTarea = (index) => {
+    tareas.value.splice(index, 1);
+}
+</script>
+<style>
+</style>
